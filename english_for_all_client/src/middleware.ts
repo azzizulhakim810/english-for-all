1bafb11440256b32c2fd45841483b1c605ed9a89
azzizulhakim810 <azzizul.hakim202@gmail.com>
2024-06-18 10:40:29 +0600

    make the api public

--- english_for_all_client/src/middleware.ts
+++ english_for_all_client/src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/about"],
+  publicRoutes: ["/", "/about", "/api/webhooks/clerk"],
 });
 
 export const config = {
